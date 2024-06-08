fe57d602b3ce96abc1182e9ca9d981db3d4c8bfa
SerigneFallouNiang <fallouniang776@gmail.com>
2024-06-08 14:08:57 +0000

    style interface detail

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -15,7 +15,7 @@ class ArticleController extends Controller
     }
 
     public function detail($id){
-        $article=Article::find($id);        
+        $article=Article::find($id);
         return view ('articles.detail' , compact('article'));
     }
 
@@ -26,9 +26,9 @@ class ArticleController extends Controller
 
     public function ajouter_article_traitement(Request $request)
     {
-     
+
         $request->validate([
-            
+
             'nom'  => 'required',
             'description'  => 'required',
             'date_création'  => 'required',
@@ -46,7 +46,7 @@ class ArticleController extends Controller
 
         $article->save();
 
-        return redirect('/ajouter')->with('status','L\'article a bien été ajouté avec succes.');
+        return redirect('/article')->with('status','L\'article a bien été ajouté avec succes.');
     }
 
     public function delete_article($id)
@@ -64,14 +64,14 @@ class ArticleController extends Controller
     public function update_article_traitement(Request $request){
 
         $request->validate([
-            
+
             'nom'  => 'required',
             'description'  => 'required',
             'date_création'  => 'required',
             'photo'  => 'required',
             'valider'  => 'required',
         ]);
-     
+
         $article= Article::find($request->id);
             $article->nom = $request->nom;
             $article->description = $request->description;
@@ -80,8 +80,8 @@ class ArticleController extends Controller
             $article->valider= $request->valider;
 
             $article->update();
-    
+
             return redirect('/article')->with('status', 'L\'article a bien été modifié avec succès.');
-    
+
     }
 }
